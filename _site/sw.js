0432be36ac6edebb3d51a7ad917a624cb67a1d7b
gvg <granttv8@gmail.com>
2023-01-18 19:08:44 -0500

    commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230118.190302';
+const cacheName = 'chirpy-20230118.190626';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
