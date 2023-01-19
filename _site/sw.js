0605d5a1025f8ac6ee1afa7817f89e2bafe58339
gvg <granttv8@gmail.com>
2023-01-19 11:52:49 -0500

    changing share

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230119.115141';
+const cacheName = 'chirpy-20230119.115224';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
