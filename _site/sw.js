97af03a244d43b320c65eef5edd40a8d6a4fc2f8
gvg <granttv8@gmail.com>
2023-01-18 19:37:40 -0500

    changes

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230118.190626';
+const cacheName = 'chirpy-20230118.193726';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
